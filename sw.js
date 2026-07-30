39c80c65de71fd95d951bb98f2eabd89d2e2bd57
mndavidflores-commits <mndavidflores@gmail.com>
2026-07-30 15:03:43 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,6 +17,7 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
     fetch(event.request)
       .then(response => {
